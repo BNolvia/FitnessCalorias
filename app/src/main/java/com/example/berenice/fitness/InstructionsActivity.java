07062129a3d4dbdec94168c89f56166c257b47fb
BNolvia <nolvia.castro@ujcv.edu.hn>
2017-03-08 12:51:55 -0600

    Layouts de Progreso y Rutina de Ejercicio

---
+++ app/src/main/java/com/example/berenice/fitness/InstructionsActivity.java
@@ -0,0 +1,16 @@
+package com.example.berenice.fitness;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Berenice on 08/03/2017.
+ */
+
+public class InstructionsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
---
+++ app/src/main/java/com/example/berenice/fitness/LevelActivity.java
@@ -0,0 +1,58 @@
+package com.example.berenice.fitness;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.example.berenice.fitness.domain.Level;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Berenice on 08/03/2017.
+ */
+
+public class LevelActivity extends Activity {
+    ListView levelList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level);
+
+        List<Level> levels = new ArrayList<Level>(){{
+            add(new Level(){{
+                title = "Novice";
+                description = getString(R.string.novice);
+            }});
+            add(new Level(){{
+                title = "Average";
+                description = getString(R.string.average);
+            }});
+            add(new Level(){{
+                title = "Above Average";
+                description = getString(R.string.above_average);
+            }});
+        }};
+
+        levelList = (ListView)findViewById(R.id.level_list);
+        levelList.setAdapter(new LevelAdapter(this, R.layout.level_row, levels));
+
+        levelList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                Bundle extras = new Bundle();
+                extras.putInt("level", (position + 1));
+                intent.putExtras(extras);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
